c2307d9b0e71da02764306b997693e9412aa00f1
jonasmacsc <vemevesc@gmail.com>
2025-08-31 14:13:53 -0300

    Update env.js

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,1 +1,7 @@
-
+// === CONFIGURAÇÃO (edite aqui) ===
+window.SC_ENV = {
+  ODDS_API_KEY: "SUA_CHAVE_THE_ODDS_API", // coloque sua chave
+  TELEGRAM_BOT_TOKEN: "", // opcional
+  TELEGRAM_CHAT_ID: "",   // opcional
+  TELEGRAM_ENABLED_DEFAULT: false
+};
